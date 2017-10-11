5cd55176c41f85daef767c70e1503c1c26664594
Kayla Hansen <shiverr@gmail.com>
2017-10-11 03:49:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,7 @@ if (array.count == 1)
 elsif (array.count == 2)
   return array.join(" and ")
 elsif (array.count >= 3 )
-  i = array.index {|el| el =~ /\d{2}/ }
-  array[0..i-1].join(", ")
-  array[i].join(" and ")
-  return array
+  return array.to_sentence
 else
   return array
 end
